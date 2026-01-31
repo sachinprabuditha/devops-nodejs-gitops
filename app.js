6119541dfb6a67ea384c07c620b0738aad7d54e3
Sachin Prabuditha <125368889+sachinprabuditha@users.noreply.github.com>
2026-01-31 10:33:59 +0530

    Initial Node.js app with Docker

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const client = require("prom-client");
+
+const app = express();
+const port = 3000;
+
+client.collectDefaultMetrics();
+
+app.get("/", (req, res) => {
+  res.send("Hello DevOps");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
